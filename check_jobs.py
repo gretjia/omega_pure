2e90bfd5989b0958cbf2f85bb9c1095b4cefe575
Zephryj Architect <zephryj@omega-vm.local>
2026-03-16 11:36:13 +0000

    chore: force index update for archived files

--- check_jobs.py
+++
@@ -1,6 +0,0 @@
-from google.cloud import aiplatform
-aiplatform.init(project="gen-lang-client-0250995579", location="us-central1")
-jobs = aiplatform.HyperparameterTuningJob.list(order_by="create_time desc")
-if jobs:
-    print(f"Latest Job Name: {jobs[0].name}")
-    print(f"State: {jobs[0].state}")
